6e48495d92e22641ef92d778bcd44e0a614302bd
grzegorz.kwasniewski <grzegorz.kwasniewski2@gmail.com>
2018-07-17 11:00:54 +0200

    ADD Basic Implementation For Recording Activity

---
+++ app/src/main/java/com/example/grzegorzkwasniewski/speakloududacity/recordingView/RecordingActivity.java
@@ -0,0 +1,31 @@
+package com.example.grzegorzkwasniewski.speakloududacity.recordingView;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.example.grzegorzkwasniewski.speakloududacity.R;
+
+/**
+ * Created by grzegorz.kwasniewski on 2018-07-17.
+ */
+
+public class RecordingActivity extends AppCompatActivity {
+
+    // region Class Constants
+    private static final String LOG_TAG = RecordingActivity.class.getSimpleName();
+    // endregion
+
+    // region View State
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+    // endregion
+}
