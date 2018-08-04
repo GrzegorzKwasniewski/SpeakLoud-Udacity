25bb315392947ddd3bd888d8f3734a630eef46f0
Grzegorz Kwasniewski <contact@grzegorzkwasniewski.com>
2018-08-04 11:13:23 +0200

    ADD Simple Add On Main Screen

--- app/src/main/java/com/example/grzegorzkwasniewski/speakloududacity/mainView/MainActivity.java
+++ app/src/main/java/com/example/grzegorzkwasniewski/speakloududacity/mainView/MainActivity.java
@@ -11,6 +11,10 @@ import com.example.grzegorzkwasniewski.speakloududacity.R;
 import com.example.grzegorzkwasniewski.speakloududacity.audioFilesView.AudioFilesActivity;
 import com.example.grzegorzkwasniewski.speakloududacity.recordingView.RecordingActivity;
 import com.example.grzegorzkwasniewski.speakloududacity.settingsView.SettingsActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -35,11 +39,19 @@ public class MainActivity extends AppCompatActivity {
     private Intent mIntent;
 
     // endregion
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         ButterKnife.bind(this);
 
